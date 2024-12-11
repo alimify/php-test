a4158bb20a2c487e557fd9b609076959d1a4713f
Abdul Alim <alimifypro@gmail.com>
2024-12-11 18:53:39 +0600

    started

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+?>
+
+<form action="">
+    <div>
+        <label></label>
+        <input type=""/>
+    </div>
+    <div>
+        <label></label>
+        <input type=""/>
+    </div>
+    <div>
+        <label></label>
+        <input type=""/>
+    </div>
+</form>
